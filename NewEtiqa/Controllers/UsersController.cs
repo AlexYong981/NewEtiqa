ff88ed556243258d9cb22c71f4ed8763175c0b5b
AlexYONG <AlexYONG@MY0134-PC>
2024-03-15 18:04:35 +0800

    Add project files.

---
+++ NewEtiqa/Controllers/UsersController.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewEtiqa.Models;
+using NewEtiqa.Data;
+
+namespace NewEtiqa.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserDbContext _context;
+
+        public UsersController(UserDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create/ Edit User
+        #region
+        [HttpPost]
+        public JsonResult CreateEdit(User user)
+        {
+            if(user.Id == 0)
+            {
+                _context.Users.Add(user);
+            }
+            else
+            {
+                var userInDb = _context.Users.Find(user.Id);
+
+                if(userInDb == null)
+                {
+                    return new JsonResult(NotFound());
+                }
+
+                userInDb = user;
+            }
+
+            _context.SaveChanges();
+
+            return new JsonResult(Ok(user));
+        }
+        #endregion
+
+        // Get User by Id
+        [HttpGet] 
+        public JsonResult GetUser(int id) 
+        { 
+            var result = _context.Users.Find(id);
+
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+
+            return new JsonResult(Ok(result));
+        }
+
+        // Delete User
+        [HttpDelete]
+        public JsonResult DeleteUser(int id) 
+        {
+            var result = _context.Users.Find(id);
+
+            if (result == null)
+            {
+                return new JsonResult(NotFound());
+            }
+
+            _context.Users.Remove(result);
+            _context.SaveChanges();
+
+            return new JsonResult(NoContent());  
+        }
+
+        // Get All User
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            var result = _context.Users.ToList();
+
+            return new JsonResult(Ok(result));
+        }
+    }
+}
---
+++ NewEtiqa/Data/UserDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NewEtiqa.Models;
+
+namespace NewEtiqa.Data
+{
+    public class UserDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public UserDbContext(DbContextOptions<UserDbContext> options)
+            :base(options)
+        {
+
+        }
+    }
+}
